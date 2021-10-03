44ee1f565d7cf55c41caa918f3417cefffde6a98
dark-red-apple <alma68@gmail.com>
2021-10-04 01:30:42 +0330

    adding js

---
+++ js/test.js
@@ -0,0 +1,11 @@
+function test() {
+    let el = document.getElementById('testcheckbox');
+    el.style.color = 'blue';
+}
+
+window.onload = function() {
+
+    console.log('Start javascript');
+    test();
+    
+};
