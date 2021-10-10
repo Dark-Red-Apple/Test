4fb1f6f5ed6e7430b7d44b308c40f0a788c07050
dark-red-apple <alma68@gmail.com>
2021-10-10 12:53:12 +0330

    Update resp.css and test.js

--- js/test.js
+++ js/test.js
@@ -1,8 +1,14 @@
+/**
+ * A test function to change DOM
+ */
 function test() {
-    let el = document.getElementById('testcheckbox');
+    let el = document.getElementById('testcheckbox1');
     el.style.color = 'blue';
 }
 
+/**
+ * Just a test javascript
+ */
 window.onload = function() {
 
     console.log('Start javascript');
